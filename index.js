e6cafadde65225a9d02eb8b1cfa057b5ec71b99d
MD Shariful Islam <79306902+sj4568@users.noreply.github.com>
2024-07-27 03:37:12 +0600

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(
   cors({
-      origin:["http://localhost:5173","https://supertyper.netlify.app"],
+      origin:["http://localhost:5174","https://supertyper.netlify.app","http://localhost:5173"],
       credentials:true
   })
 );
