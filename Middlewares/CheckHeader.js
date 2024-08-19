44f171375373ab1f643f2efc0b17200630dc72c1
MD Shariful Islam <masom3456@gmail.com>
2024-08-20 00:48:58 +0600

    erer

--- Middlewares/CheckHeader.js
+++ Middlewares/CheckHeader.js
@@ -1,12 +1,14 @@
 
 exports.checkHeader = async (req,res,next)=>{
-  console.log(req.headers);
-//   const blockUser = process.env.BLOCK_USER_AGENTS.split("___")
-//   if(blockUser.some(data=>userAgent)){
-//     res.send({
-//         massage:"un Authorized"
-//     })
-//     return
-//   }    
-    next();
+ 
+    const referer = req.headers['referer']
+
+    if(referer && referer=="https://supertyper.netlify.app/"){
+        
+        next();
+    }
+  
+    res.send({
+        massage:"Arek Joner website hack kora thik na"
+    })
 }
--- api/ResisterAndLogin/ResistrerAndLogin.js
+++ api/ResisterAndLogin/ResistrerAndLogin.js
@@ -2,13 +2,14 @@ const express = require("express")
 const { CreateUser, LoginUser, LogoutUser, UpdateUser, FindeOneUser, UpdateFollowers } = require("../../database/Controllers/User")
 const { jwtTokemGenarator } = require("../../Helper/jwtTokenGenarator")
 const { JwttokenVerify } = require("../../Middlewares/JwttokenVerify")
+const { checkHeader } = require("../../Middlewares/CheckHeader")
 
 
 const Resiterrouter = express.Router()
-Resiterrouter.post("/user/create", CreateUser)
+Resiterrouter.post("/user/create", checkHeader, CreateUser)
 Resiterrouter.post("/user/login",LoginUser)
 Resiterrouter.post("/user/logout", LogoutUser)
 Resiterrouter.put("/user/update/:id", UpdateUser)
 Resiterrouter.get("/user/:id", FindeOneUser)
-Resiterrouter.put("/user/followers/:id",UpdateFollowers)
+Resiterrouter.put("/user/followers/:id",checkHeader, UpdateFollowers)
 module.exports= Resiterrouter
